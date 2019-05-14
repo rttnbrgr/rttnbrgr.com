9a3bc94647b4ef953a4d7aa099bdf8994764c38f
Nathan Rothenberger <nroth10@gmail.com>
2019-05-13 22:22:55 -0700

    Comment the transform code. Change tilt.

--- src/transform.js
+++ src/transform.js
@@ -1,22 +1,25 @@
-console.log('woah');
+const doc = document.documentElement; // Select the document
+const card = document.querySelector('.card') // Select the card
 
-const doc = document.documentElement;
-const card = document.querySelector('.card')
-
+// Debug
 console.log("h = " + getComputedStyle(doc).width.slice(0,-2));
 console.log("w = " + getComputedStyle(doc).height.slice(0,-2));
 
 
 document.addEventListener('mousemove', function(e) {
   
+  // Get the width & height of the page  
   let w =  getComputedStyle(doc).width.slice(0,-2);
   let h =  getComputedStyle(doc).height.slice(0,-2);
   
-  const maxTilt = 45;
+  // Set max tilt for card
+  const maxTilt = 15;
   const tiltFactor = h / 2 / maxTilt;
   
+  // Calculate the transform for card
   let ry = (w / 2 - e.pageX) / 10;
   let rx = (h / 2 - e.pageY) / tiltFactor;
   
+  // Apply via inline style
   card.style.transform = `rotateX(${rx}deg) rotateY(${ry}deg`;
 });
